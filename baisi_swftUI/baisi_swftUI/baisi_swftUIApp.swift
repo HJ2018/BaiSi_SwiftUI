c2e7b005f2771c20b1394b73df5ef9bb602e4395
HJ2018 <v_v668@163.com>
2022-03-18 17:39:26 +0800

    p

---
+++ baisi_swftUI/baisi_swftUI/baisi_swftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  baisi_swftUIApp.swift
+//  baisi_swftUI
+//
+//  Created by jie.huang on 2022/3/18.
+//
+
+import SwiftUI
+
+@main
+struct baisi_swftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
